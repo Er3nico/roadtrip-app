ef70e809c7760ea617294a8bfc73cf2b986a0711
Nico Scholz <scholznico08@gmail.com>
2026-07-25 19:52:02 +0200

    Store-Screenshots ergänzen
    Zwei Screenshots (Route- und Hotels-Ansicht mit Beispieldaten) im
    Manifest referenziert, Service-Worker-Cache auf v3 angehoben.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'roadtrip-app-v2';
+const CACHE_NAME = 'roadtrip-app-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
