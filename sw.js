7de6d893e7c36aa03e20b8cfed5cd221e6e3951a
Nico Scholz <scholznico08@gmail.com>
2026-07-26 00:17:52 +0200

    Etappen beim Öffnen eines Trips standardmäßig einklappen
    Alle bestehenden Etappen starten jetzt eingeklappt, sobald ein Trip
    geöffnet wird - für bessere Übersicht bei vielen Etappen. Neu über
    "+ Etappe hinzufügen" angelegte Etappen bleiben weiterhin
    automatisch aufgeklappt, damit man sie direkt ausfüllen kann.
    
    Service-Worker-Cache auf v20.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'roadtrip-app-v19';
+const CACHE_NAME = 'roadtrip-app-v20';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
