36b016b50dd8a6619dec7077ba40a28ce3707db5
Nico Scholz <scholznico08@gmail.com>
2026-07-25 20:56:01 +0200

    Bottom-Navigation für Hauptbereiche einführen
    Route/Sehenswürdigkeiten/Hotels bekommen eine eigene Navigationsleiste
    am unteren Bildschirmrand statt im Hamburger-Menü. Das Drawer-Menü
    bleibt für Zusatzfunktionen (Export/Import/Löschen/Info). Service-
    Worker-Cache auf v6.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'roadtrip-app-v5';
+const CACHE_NAME = 'roadtrip-app-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
